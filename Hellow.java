5b4337fe7d08f92a2631951433e3689415e9d493
mehedi <bd34017053@gmail.com>
2021-12-29 07:12:23 +0600

    Second java

--- Hellow.java
+++ Hellow.java
@@ -1,6 +1,14 @@
 public static Hellow{
     public static void main(String[] args)
     {
-        System.out.Println("Hellow Mehedi Hasan");
+        //System.out.Println("Hellow Mehedi Hasan");
+
+        name();
+    }
+
+    public static string name()
+    {
+        String p="Take this";
+        retrun p;
     }
 }
