0a177b54c60fc83b4ab876c4a5fa822f23eda3b6
mehedi <bd34017053@gmail.com>
2021-12-29 07:22:16 +0600

    Third change

---
+++ Hellow.java
@@ -0,0 +1,22 @@
+public static Hellow{
+    public static void main(String[] args)
+    {
+        //System.out.Println("Hellow Mehedi Hasan");
+
+        name();
+
+        System.out.Println(put());
+    }
+
+    public static string name()
+    {
+        String p="Take this";
+        retrun p;
+    }
+
+    public static int put()
+    {
+        int w-2+3;
+        return w;
+    }
+}
